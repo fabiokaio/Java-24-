a6545fbfd82dc1a9f8da1f2b2ea0ccae022b0f20
Fábio Kaio <fabiokaio501@gmail.com>
2025-05-23 13:45:35 +0000

    Anotações me comentários.

--- Main.java
+++ Main.java
@@ -1,3 +1,18 @@
-public static void main(String[] args) {
-    
-}
+// public static void main(String[] args) {
+
+// }
+
+//Primeiro  código em Java
+//VARIÁVEIS ABAIXO NÃO FORAM DECLARADAS . SERVEM APENAS PARA FINS DE ENTENDIMENTO.
+// public class Main {
+//     public static void main (String[] args){
+//         // Declarando variáveis com var :
+//         var Nome = "Kaio";
+//         var idada = 18;
+//         // Declarando Variaáveis com o seu determinado tipo:
+//         String meuNome = "João";
+//         int Idd = 28;
+
+//         System.out.println ("Meu nome é");
+//     }
+// }
